763a49d0a2f666a5c5ef8e5eb0fcc471b5402476
hojenahistoria2026 <hojenahistoria2026@gmail.com>
2026-08-16 00:14:42 +0100

    Update calendario.js

--- calendario.js
+++ calendario.js
@@ -1,81 +1,78 @@
 const meses = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro"
+"Janeiro",
+"Fevereiro",
+"Março",
+"Abril",
+"Maio",
+"Junho",
+"Julho",
+"Agosto",
+"Setembro",
+"Outubro",
+"Novembro",
+"Dezembro"
 ];
 
 const hoje = new Date();
 
 const ano = hoje.getFullYear();
+
 const mes = hoje.getMonth();
+
 const diaAtual = hoje.getDate();
 
 document.getElementById("mesAno").innerHTML =
-    meses[mes] + " " + ano;
+meses[mes] + " " + ano;
 
-const diasContainer = document.getElementById("dias");
+const diasContainer =
+document.getElementById("dias");
 
 const primeiroDia =
-    new Date(ano, mes, 1).getDay();
+new Date(ano, mes, 1).getDay();
 
 const diasMes =
-    new Date(ano, mes + 1, 0).getDate();
+new Date(ano, mes + 1, 0).getDate();
 
 let inicio = primeiroDia - 1;
 
-if (inicio < 0) {
-    inicio = 6;
+if(inicio < 0){
+
+inicio = 6;
+
 }
 
-// Espaços antes do primeiro dia do mês
-for (let i = 0; i < inicio; i++) {
+for(let i=0;i<inicio;i++){
 
-    const vazio = document.createElement("div");
+const vazio=document.createElement("div");
 
-    diasContainer.appendChild(vazio);
+diasContainer.appendChild(vazio);
+
 }
 
-// Criar os dias
-for (let dia = 1; dia <= diasMes; dia++) {
+for(let dia=1;dia<=diasMes;dia++){
 
-    const div = document.createElement("div");
+const div=document.createElement("div");
 
-    div.classList.add("dia");
+div.classList.add("dia");
 
-    div.innerHTML = dia;
+div.innerHTML=dia;
 
-    // Destacar o dia atual
-    if (dia === diaAtual) {
-        div.classList.add("hoje");
-    }
+if(dia===diaAtual){
 
-    // Quando clicar num dia
-    div.onclick = function () {
+div.classList.add("hoje");
 
-        let d = String(dia).padStart(2, "0");
-        let m = String(mes + 1).padStart(2, "0");
+}
 
-        // Informação disponível
-        if (m === "08" && d >= "17" && d <= "21") {
+div.onclick=function(){
 
-            window.location.href = d + "-" + m + ".html";
+let d=String(dia).padStart(2,"0");
 
-        } else {
+let m=String(mes+1).padStart(2,"0");
 
-            alert("Ainda não existe informação para este dia.");
+window.location.href="dia.html?data="+d+"-"+m;
 
-        }
+}
 
-    };
+diasContainer.appendChild(div);
 
-    diasContainer.appendChild(div);
 }
